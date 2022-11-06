9a72d278cdefbff1ba46bb616b7833059101010e
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-11-05 20:47:41 -0500

    reorg folders; added syntax to post layout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
-  res.render('index');
+  res.render('main');
 });
 
 module.exports = router;
--- models/post.js
+++ models/post.js
@@ -5,14 +5,34 @@ class Post extends Model {}
 
 Post.init(
   {
-
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'post',
-  }
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    posting_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+   },
+   {
+     sequelize,
+     freezeTableName: true,
+     underscored: true,
+     modelName: 'post',
+   }
 );
-
 module.exports = Post;
