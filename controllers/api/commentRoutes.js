07565bc3df71fbc5de6fb65fe3a0354804a48765
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-11-06 23:24:32 -0600

    set up comment route

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+   try {
+     const dbCommentData = await Comment.findAll().catch((err) => { 
+       res.json(err);
+     });
+     res.status(200).json(dbCommentData);
+   } catch (err) {
+     console.log(err);
+     res.status(500).json(err);
+   }
+ });
+
+router.post('/', withAuth, async (req, res) => {
+  // check the session
+  if (req.session) 
+  try {
+   const commentData = await Comment.create({
+   comment_content: req.body.comment_content,
+   post_id: req.body.post_id,
+   comment_date: req.body.comment_date,
+   user_id: req.session.user_id,
+  })
+  // if the comment is successfully created, the new response will be returned as json
+  res.status(200).json(commentData)
+} catch (err) {
+  res.status(400).json(err);
+}
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+   try {
+      const dbCommentData = await Comment.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comments found with this id!' });
+        return;
+      }
+      res.status(200).json(dbCommentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const entryRoutes = require('./entryRoutes');
 const userRoutes = require('./userRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
 router.use('/entries', entryRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
